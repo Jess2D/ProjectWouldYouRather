367977cc45373a4650800d7cddc5d3aa9af40898
Jessica Silveira <jessicap_silveira@hotmail.com>
2022-05-18 08:22:48 +1200

    Add bare bones to the app

--- src/components/page/component/Page.tsx
+++ src/components/page/component/Page.tsx
@@ -1,21 +1,47 @@
 import React from 'react';
-import '../view/page.css'
-import avatar from "../../../assets/img/avatars/Avatar1.png"
+import '../view/page.css';
+import avatar from "../../../assets/img/avatars/Avatar1.png";
+import home from "../../../assets/icons/home.png";
+import question from "../../../assets/icons/question.png";
+import leader from "../../../assets/icons/leader.png";
+import logout from "../../../assets/icons/logout.png";
+
 
 function Page() {
   return (
-    <div className="App">
-    <div className="sidebar">
+    <div className="app">
+    <div className="sidebar"  >
         <div className="avatar">
             <img src={avatar} alt="user avatar"/>
         </div>
-        <div className="user-greetings"></div>
-        <div className="menu"></div>
+        <div className="user-greetings">Hello, Tyler McGinnes</div>
+        <nav className="menu">
+          <ul>
+            <li className="item">
+              <div className="icon"> <img src={home} alt="home icon"/></div>
+              <div className="item-link">Home</div>
+            </li>
+            <li className="item">
+              <div className="icon"> <img src={question} alt="question icon"/></div>
+              <div className="item-link">New Question</div>
+            </li>
+            <li className="item">
+              <div className="icon"><img src={leader} alt="leader ranking icon"/></div>
+              <div className="item-link">Leader Board</div>
+            </li>
+            <li className="item">
+              <div className="icon"><img src={logout} alt="logout icon"/></div>
+              <div className="item-link">Logout</div>
+            </li>
+          </ul>
+        </nav>
     </div>
     <div className="page-content">
-        <header className="App-header">
+        <div className="app-header">
             <h1>Would Rather game</h1>
-        </header>
+        </div>
+           
+   
         <div className="main"></div>
     </div>
 
